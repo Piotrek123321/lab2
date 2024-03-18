3f09a05428984c19773a39a1d0c9395c10243aca
Piotr Żuławnik <piotr.zulawnik14@gmail.com>
2024-03-18 19:23:53 +0100

    zad2

--- views/home.js
+++ views/home.js
@@ -6,8 +6,23 @@ function renderPage() {
       <title>home.js</title>
     </head>
     <body>
-      <div>home.js</div>
+      <div>
+        <form>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="text" name="lastname" placeholder="Lastname" />
+          <input type="number" name="age" placeholder="Age" />
+          <select name="gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          <input type="number" name="Code" placeholder="Code" />
+          <input type="text" name="studyField" placeholder="Study Field" />
+          <input type="submit" value="Submit">
+        </form>
+      </div>
     </body>
     </html>`;
   }
+  
   module.exports = renderPage;
